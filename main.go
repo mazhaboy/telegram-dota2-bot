2a2529be761c8679d3df5047b4448b6a52c992df
mazhaboy <mazhitov.t@list.ru>
2020-11-27 14:03:01 +0600

    bot

--- main.go
+++ main.go
@@ -2,41 +2,35 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"math/rand"
-	"net/http"
 	"os"
 	"time"
 
 	tb "gopkg.in/tucnak/telebot.v2"
 )
 
+var (
+	port      = os.Getenv("PORT")       // sets automatically
+	publicURL = os.Getenv("PUBLIC_URL") // you must add it to your config vars
+	token     = os.Getenv("TOKEN")      // you must add it to your config vars
+)
+
 func main() {
-	port := os.Getenv("PORT")
-	http.HandleFunc("/", bot)
-	log.Print("Listening on:" + port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
-
-}
-func bot(w http.ResponseWriter, r *http.Request) {
-
-	io.WriteString(w, "Hello bot!")
-	b, err := tb.NewBot(tb.Settings{
-		// You can also set custom API URL.
-		// If field is empty it equals to "https://api.telegram.org".
-		URL: "https://api.telegram.org",
-
-		Token:  "1476386207:AAEG5kTR8KL2A3xJN5xmGF5ODVwTxxnZWT4",
-		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
-		// https://api.telegram.org1476386207:AAEG5kTR8KL2A3xJN5xmGF5ODVwTxxnZWT4/getUpdates
-	})
-
-	if err != nil {
-		log.Fatal(err)
-		return
+	//https://api.telegram.org1476386207:AAEG5kTR8KL2A3xJN5xmGF5ODVwTxxnZWT4
+	webhook := &tb.Webhook{
+		Listen:   ":" + port,
+		Endpoint: &tb.WebhookEndpoint{PublicURL: publicURL},
+	}
+	pref := tb.Settings{
+		Token:  token,
+		Poller: webhook,
 	}
 
+	b, err := tb.NewBot(pref)
+	if err != nil {
+		log.Fatal(err)
+	}
 	b.Handle("/hello", func(m *tb.Message) {
 		b.Reply(m, "Hello dotadrochers😈\n\nYou can control me by sending these commands:\n\n/hello - info about me.\n/start  - this command run process which sends voting poll in random time during the next 5 days from the moment it started.\n(use only one time its recursive process)")
 	})
@@ -47,8 +41,10 @@ func bot(w http.ResponseWriter, r *http.Request) {
 	}
 
 	poll.AddOptions("Yes", "No")
-	max := 432000
-	min := 86400
+	// max := 432000
+	// min := 86400
+	max := 50
+	min := 10
 
 	b.Handle("/start", func(m *tb.Message) {
 		for {
@@ -63,7 +59,9 @@ func bot(w http.ResponseWriter, r *http.Request) {
 	})
 
 	b.Start()
+
 }
+
 func random(min int, max int) int {
 	return rand.Intn(max-min) + min
 }
